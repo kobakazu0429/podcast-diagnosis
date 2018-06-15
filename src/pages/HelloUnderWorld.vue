17903794f3eb099917e1189299a8e253b01b41b4
kobakakzu0429 <kobakazu10@gmail.com>
2018-06-15 21:12:06 +0900

    vue init

---
+++ src/pages/HelloUnderWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Under Ground'
+    };
+  }
+};
+</script>
+
+<style scoped>
+.hello {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+</style>
