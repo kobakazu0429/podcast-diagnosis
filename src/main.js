4521c2f9aa15e322477b8894b5fb9b83ce165f17
kobakakzu0429 <kobakazu10@gmail.com>
2018-06-18 23:31:20 +0900

    vuexの移動&なんか色々

--- src/main.js
+++ src/main.js
@@ -1,54 +1,24 @@
 import Vue from 'vue';
 import App from './App';
-import Vuex from 'vuex';
 import router from './router';
-
-Vue.use(Vuex);
+import store from './store';
 
 Vue.config.productionTip = false;
+Vue.config.devtools = true;
+
+// -------------
+// Component
+import RadarChart from '@/components/radar-chart';
+import CheckboxButton from '@/components/checkbox-button';
+
+Vue.component('radar-chart', RadarChart);
+Vue.component('checkbox-button', CheckboxButton);
 
 /* eslint-disable no-new */
-const store = new Vuex.Store({
-  state: {
-    class_a_1: true,
-    class_a_2: true,
-    class_a_3: true,
-    class_a_4: true,
-    class_a_5: true,
-    class_b_1: true,
-    class_b_2: true,
-    class_b_3: true,
-    class_b_4: true,
-    class_b_5: true,
-    class_c_1: true,
-    class_c_2: true,
-    class_c_3: true,
-    class_c_4: true,
-    class_c_5: true,
-    class_e_1: true,
-    class_e_2: true,
-    class_e_3: true,
-    class_e_4: true,
-    class_e_5: true,
-    class_f_1: true,
-    class_f_2: true,
-    class_f_3: true,
-    class_f_4: true,
-    class_f_5: true
-  },
 
-  getters: {
-    getClassA1(state) {
-      return state.class_a_1;
-    }
-  },
-
-  mutations: {
-    updateChecked(state, obj) {
-      console.log(obj);
-      state[obj.key] = obj.value;
-    }
-  }
+window.addEventListener('touchmove', function(event) {
+  console.log('hoge');
+  event.preventDefault();
 });
 
 new Vue({
