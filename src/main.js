5974b6d2b6c3f65d82a07d9a1c53875033c2d9b1
kobakakzu0429 <kobakazu10@gmail.com>
2018-07-15 21:18:08 +0900

    add vue-analytics setting

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
+
+Vue.use(VueAnalytics, {
+  id: 'UA-122380472-1',
+  router
+});
 
 // -------------
 // Component
